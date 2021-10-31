488acf9f2ad4f33648977e6c5fa7ac48103de728
Matheus Souza <matheus.ramirez019@gmail.com>
2021-10-31 15:41:17 -0300

    static web page

---
+++ web/src/components/LoginBox/index.tsx
@@ -0,0 +1,18 @@
+import { useContext } from 'react';
+import { VscGithubInverted } from 'react-icons/vsc'
+
+
+import styles from './styles.module.scss';
+
+export function LoginBox() {
+
+  return (
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre e compartilhe sua mensagem</strong>
+      <a href="/" className={styles.signInWithGithub}>
+        <VscGithubInverted size="24" />
+        Entrar com Github
+      </a>
+    </div>
+  )
+}
